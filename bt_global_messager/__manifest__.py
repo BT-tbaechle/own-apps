73800c65d95382272385ab7a858df5f04b06ee49
Tobias Baechle <tobias.baechle@braintec-group.com>
2017-08-29 06:35:39 +0200

    Initial commit for bt_global_messager

---
+++ bt_global_messager/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- # pylint: disable=C0111
+##############################################################################
+#
+#    Copyright (c) 2017 brain-tec AG (http://www.braintec-group.com)
+#    All Right Reserved
+#
+#    See LICENSE file for full licensing details.
+##############################################################################
+{
+    'name': "BT Global Messager",
+    'summary': "Module that allows instant or time-based messaging with users.",
+    'author': "braintec AG",
+    'license': 'LGPL-3',
+    'version': '10.0.0',
+    'category': 'Extra Tools',
+    'website': 'http://www.braintec-group.com',
+    'images': [
+    ],
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
